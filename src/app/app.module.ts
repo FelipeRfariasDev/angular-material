518ac71945f47cb41aace14565c30e46d3e1795b
Felipe <felipe.farias.php@gmail.com>
2022-03-02 17:51:29 -0300

    PostsAddComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsAddComponent } from './posts/components/posts-add/posts-add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsAddComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/components/posts-add/posts-add.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-add',
+  templateUrl: './posts-add.component.html'
+})
+export class PostsAddComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
