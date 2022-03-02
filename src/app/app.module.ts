11408ec578b9ceef276eca8fe900703d71f73ba2
Felipe <felipe.farias.php@gmail.com>
2022-03-02 18:28:54 -0300

    app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PostsAddComponent } from './posts/components/posts-add/posts-add.component';
-import { PostsUpdateComponent } from './posts/components/posts-update/posts-update.component';
-import { PostsDetailsComponent } from './posts/components/posts-details/posts-details.component';
-import { PostsListsComponent } from './posts/components/posts-lists/posts-lists.component';
-import { LoginComponent } from './authentication/components/login/login.component';
-import { NewUserComponent } from './authentication/components/new-user/new-user.component';
 import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsAddComponent,
-    PostsUpdateComponent,
-    PostsDetailsComponent,
-    PostsListsComponent,
-    LoginComponent,
-    NewUserComponent,
     MenuComponent
   ],
   imports: [
