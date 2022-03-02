66a9384750e4e7cf75510c64a2ffbba29737b031
Felipe <felipe.farias.php@gmail.com>
2022-03-02 17:53:00 -0300

    PostsUpdateComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsAddComponent } from './posts/components/posts-add/posts-add.component';
+import { PostsUpdateComponent } from './posts/components/posts-update/posts-update.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsAddComponent
+    PostsAddComponent,
+    PostsUpdateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/posts/components/posts-update/posts-update.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-posts-update',
+  templateUrl: './posts-update.component.html'
+})
+export class PostsUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
