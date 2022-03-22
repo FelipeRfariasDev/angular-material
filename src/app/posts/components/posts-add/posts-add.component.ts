f18d68ad8bb8b9330178067704cd8ef442b0a656
Felipe <felipe.farias.php@gmail.com>
2022-03-22 20:44:19 -0300

    add post

--- src/app/posts/components/posts-add/posts-add.component.ts
+++ src/app/posts/components/posts-add/posts-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostsService } from '../../service/posts.service';
 
 @Component({
   selector: 'app-posts-add',
@@ -6,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsAddComponent implements OnInit {
 
-  constructor() { }
+  selectedFile: File | undefined;
+
+  form = new FormGroup({
+    titulo: new FormControl('', Validators.required),
+    descricao: new FormControl('', Validators.required)
+  });
+
+  constructor(private postservice: PostsService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+  
+  add() {
+    const formData = new FormData();
+    formData.append('titulo', this.form.get('titulo')?.value);
+    formData.append('descricao', this.form.get('descricao')?.value);
+    formData.append('imagem', this.selectedFile || '');
+
+    this.postservice.post(formData).subscribe((response:any)=>{
+      if(response.success){
+        this.router.navigateByUrl('/list-posts');
+        return;
+      }
+    });
+  }
+
+  inputFileChange(event: Event) {
+    const element = event.target as HTMLInputElement;
+    if (element.files && element.files[0]) {
+      this.selectedFile = element.files[0];
+    }
   }
 
 }
