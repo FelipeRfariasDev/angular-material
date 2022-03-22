ef111e6767c8f8e473facf8aa73ed702f1bfff78
Felipe <felipe.farias.php@gmail.com>
2022-03-22 19:59:22 -0300

    listar posts com paginação

--- src/app/posts/components/posts-lists/posts-lists.component.ts
+++ src/app/posts/components/posts-lists/posts-lists.component.ts
@@ -4,12 +4,36 @@ import { Router } from '@angular/router';
 import { Post } from '../../model/post';
 import { PostsService } from '../../service/posts.service';
 
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
+
 @Component({
   selector: 'app-posts-lists',
   templateUrl: './posts-lists.component.html',
   styleUrls: ['./posts-lists.component.css']
 })
 export class PostsListsComponent implements OnInit {
+
+  displayedColumns: string[] = ['id','titulo','descricao','imagem','btn'];
+  dataSource = ELEMENT_DATA;
 
   posts: Post[] = [];
   links:any="";
@@ -30,9 +54,25 @@ export class PostsListsComponent implements OnInit {
     this.getPaginate();
   }
 
-  getPaginate(){
+  getPaginate(url:any=null) {
+    
     var valorBuscar = this.form.value.buscar;
-    console.log(valorBuscar);
+
+    this.postsService.getPaginate(url,valorBuscar).subscribe((response: any) => {
+      this.posts = response.posts.data;
+      this.links = response.posts.links;
+      
+      if(!url){
+        url = response.posts.path;
+      }
+      this.link_page_atual = url;
+    }, error => {
+      if(error.error.success==false || error.error.status==403){
+        localStorage.setItem('accessToken','');
+        //this.toastr.error(error.error.message,"Erro");
+        this.router.navigate(['/']);
+      }
+    });
   }
 
 }
